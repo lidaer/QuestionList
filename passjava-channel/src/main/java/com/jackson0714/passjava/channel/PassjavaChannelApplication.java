7a9f5180056e1c53a58e16cd101e1c4f37563c37
fdse <2795656376@qq.com>
2023-01-08 17:59:40 +0800

    Spring Cloud整合Nacos注册中心

--- passjava-channel/src/main/java/com/jackson0714/passjava/channel/PassjavaChannelApplication.java
+++ passjava-channel/src/main/java/com/jackson0714/passjava/channel/PassjavaChannelApplication.java
@@ -2,7 +2,9 @@ package com.jackson0714.passjava.channel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class PassjavaChannelApplication {
 
--- passjava-question/src/main/java/com/jackson0714/passjava/question/PassjavaQuestionApplication.java
+++ passjava-question/src/main/java/com/jackson0714/passjava/question/PassjavaQuestionApplication.java
@@ -3,7 +3,9 @@ package com.jackson0714.passjava.question;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("com.jackson0714.passjava.question.dao")
 public class PassjavaQuestionApplication {
