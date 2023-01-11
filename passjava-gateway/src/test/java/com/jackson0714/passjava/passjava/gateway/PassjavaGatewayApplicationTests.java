7923adb82630a751be6f0a5c5755c03bdf54094c
fdse <2795656376@qq.com>
2023-01-12 00:08:57 +0800

    Spring Cloud整合Gateway网关

---
+++ passjava-gateway/src/test/java/com/jackson0714/passjava/passjava/gateway/PassjavaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackson0714.passjava.passjava.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassjavaGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
